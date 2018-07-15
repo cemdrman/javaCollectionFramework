07dfe64dca0b5282cec6db99e5e374fc4b9f0e9a
cem dırman <cemdrman@gmail.com>
2018-07-15 23:59:54 +0300

    vector

--- src/javacollectionframework/collections/list/vector/TestVector.java
+++ src/javacollectionframework/collections/list/vector/TestVector.java
@@ -5,10 +5,27 @@
  */
 package javacollectionframework.collections.list.vector;
 
+import java.util.List;
+import java.util.Vector;
+
 /**
  *
  * @author cemdirman
  */
 public class TestVector {
-    
+    public static void main(String[] args) {
+        List vector = new Vector();
+        vector.add("Fenerbahçe");
+        vector.add(23);
+        
+        
+        Vector<String> vector1 = new Vector<>(10);
+        for (int i = 0; i < vector1.size(); i++) {
+            vector1.add(i + ".sayi");
+        }
+        vector1.setSize(20);       
+        
+        
+        Vector<Character> vector2 = new Vector<>(30, 10);
+    }
 }
